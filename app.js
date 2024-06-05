ecd41a471621b4a44b4181c6e744024f9578b798
BAthiphong <Athiphong.bunkhu@gmail.com>
2024-06-05 22:18:59 +0700

    :sparkles: New template.

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req,res) => {
 
@@ -15,6 +17,6 @@ app.get("/", (req,res) => {
 })
 
 app.listen(port, ()=>{
-  console.log('Listening on port' + chalk.green(port));
+  console.log('Listening on port ' + chalk.green(port));
   debug('Listening on port' + chalk.green(port));
 })
