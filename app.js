b060f5590724dcf1c4f785ca433157bc65f9d344
BAthiphong <Athiphong.bunkhu@gmail.com>
2024-06-05 21:38:03 +0700

    :sparkles: Add all file project.

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get("/", (req,res) => {
+
+  res.send('Hellow Fans');
+
+})
+
+app.listen(port, ()=>{
+  console.log('Listening on port' + chalk.green(port));
+  debug('Listening on port' + chalk.green(port));
+})
